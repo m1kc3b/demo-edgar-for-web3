11aa1827d81945a18ddf0f7813c5a39073418043
m1kc3b <cebulski.mi@gmail.com>
2025-06-16 12:55:58 +0200

    updates: removed greetings function

--- contracts/Counter.sol
+++ contracts/Counter.sol
@@ -3,11 +3,9 @@ pragma solidity ^0.8.20;
 
 contract Counter {
     uint public count;
-    string public greeting;
 
     constructor() {
         count = 0;
-        greeting = "Hello, Web3!";
     }
 
     function increment() public {
@@ -20,13 +18,5 @@ contract Counter {
 
     function getCount() public view returns (uint) {
         return count;
-    }
-
-    function setGreeting(string memory _newGreeting) public {
-        greeting = _newGreeting;
-    }
-
-    function getGreeting() public view returns (string memory) {
-        return greeting;
     }
 }
